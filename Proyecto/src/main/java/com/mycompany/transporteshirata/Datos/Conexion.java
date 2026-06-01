ba184ccde1033e0b3a49f520f16ce63d19eae977
Nicolas <n.diaz115@alumnos.santotomas.cl>
2026-06-01 01:19:05 -0400

    Se añadio

--- Proyecto/src/main/java/com/mycompany/transporteshirata/Datos/Conexion.java
+++ Proyecto/src/main/java/com/mycompany/transporteshirata/Datos/Conexion.java
@@ -22,7 +22,7 @@ public class Conexion {
     private static final String CONFIG_LOCAL = "hirata-db.properties";
     private static final String URL_DEFECTO = "jdbc:mysql://localhost:3306/Transportes_Hirata";
     private static final String USUARIO_DEFECTO = "root";
-    private static final String PASSWORD_DEFECTO = "";
+    private static final String PASSWORD_DEFECTO = "Catdog10";
     private static boolean esquemaVerificado = false;
 
     public static Connection getConexion() {
